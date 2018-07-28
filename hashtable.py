662e5f7b672bd1770b661cbaac99c0e4b7550dda
igbes <igbes900@gmail.com>
2018-07-28 11:37:29 +0800

    Тема 9. Хеширование. Добавлены тесты

--- hashtable.py
+++ hashtable.py
@@ -57,7 +57,15 @@ class TestHashTable(unittest.TestCase):
     def test_put(self):
         self.assertEqual(self.h_table.show_hashtable(), 
                          ['eD', None, None, None, None, None, None, None, None, None, None, 'AA', 'Bc', None, None, None, 'dE'])
-           
+       
+    def test_seek_slot(self):
+        self.h_table = HashTable(4, 3)
+        self.h_table.put("AA")
+        self.h_table.put("Bc")
+        self.h_table.put("dE") 
+        self.h_table.put("eD")        
+        self.assertEqual(self.h_table.seek_slot("DD"), None) 
+        
     def test_find(self):
         self.assertEqual(self.h_table.find("dE"), 16)
         self.assertEqual(self.h_table.find("eD"), 0)
