7f90aec3159c2c2b25dc4f235b949f577a69276d
igbes <igbes900@gmail.com>
2019-03-25 21:14:42 +0800

    Singly linked list / односвязный список

---
+++ singly-linked-list/singly_linked_list.py
@@ -0,0 +1,96 @@
+
+class Node:
+    
+    def __init__(self, v):
+        self.value = v
+        self.next = None
+        
+        
+class LinkedList:
+    
+    def __init__(self):
+        self.head = None
+        self.tail = None
+        
+    def add_in_tail(self, item):
+        if self.head is None:
+            self.head = item
+        else:
+            self.tail.next = item
+        self.tail = item
+        
+    def print_all_nodes(self):
+        node = self.head
+        while node != None:
+            print(node.value)
+            node = node.next
+            
+    def find(self, val):
+        node = self.head
+        while node is not None:
+            if node.value == val:
+                return node
+            node = node.next
+        return None
+    
+    # 1.1 - 1.2
+    def delete(self, val, all=False): 
+        """Удаляет один или все узлы по значению,
+         по умолчанию удаляется первый нашедшийся элемент"""
+        node = self.head
+        preNode = Node(None)
+        while node is not None:
+            if node == self.head and node.value == val:
+                self.head = node.next
+                if all == False:
+                    return
+            elif node.value != val:
+                preNode = node
+            else:
+                preNode.next = node.next
+                if all == False:
+                    return
+            node = node.next            
+         
+    #1.3    
+    def clean(self):
+        """Очищает содержимое списка (создание пустого списка)"""
+        self.head = None
+        self.tail = None 
+        
+    #1.4 
+    def find_all(self, val):
+        """Возвращает список узлов по заданному значению"""
+        def iter(node, acc):
+            if node is None:
+                return acc
+            if node.value == val:
+                acc.append(node)
+            return iter(node.next, acc)    
+        return iter(self.head, [])
+    
+    # 1.5    
+    def len(self):
+        """Возвращает длину списка"""
+        def iter(node, acc):
+            if node is None:
+                return acc
+            return iter(node.next, acc + 1)
+        return iter(self.head, 0)
+    
+    # 1.6
+    def insert(self, afterNode, newNode):
+        """Вставляет узел после заданного узла по значению"""
+        if self.head == None:
+            self.add_in_tail(Node(newNode))
+        def iter(node):
+            if node is None:
+                return
+            if node.value == afterNode:
+                new_node = Node(newNode)
+                temp = node.next
+                node.next = new_node
+                new_node.next = temp
+                return
+            return iter(node.next)    
+        return iter(self.head)
