ffe7960bbad5355219fd9aa72288dbd13cb63f1f
igbes <igbes900@gmail.com>
2019-03-25 12:25:54 +0800

    Singly linked list / односвязный список

--- linkedlist_1.py
+++ linkedlist_1.py
@@ -1,22 +1,31 @@
+import unittest
+
 class Node:
+    
     def __init__(self, v):
         self.value = v
         self.next = None
+        
+        
 class LinkedList:
+    
     def __init__(self):
         self.head = None
         self.tail = None
+        
     def add_in_tail(self, item):
         if self.head is None:
             self.head = item
         else:
             self.tail.next = item
         self.tail = item
+        
     def print_all_nodes(self):
         node = self.head
         while node != None:
             print(node.value)
             node = node.next
+            
     def find(self, val):
         node = self.head
         while node is not None:
@@ -25,80 +34,74 @@ class LinkedList:
             node = node.next
         return None
     
-    def __iter__(self):
-        return self
-    
-    def __next__(self):
-        item = self.head
-        if item is not None:
-            return item
-        """while item.next != None:
-            yield item
-            item = item.next"""
-        
-    
     # 1.1 - 1.2
-    def remove(self, item, boolItem = True): 
-        # Удаление одного или всех узлов по значению
-        # По умолчанию удаляет все узлы по значению item
+    def delete(self, val, all=False): 
+        """Удаляет один или все узлы по значению,
+         по умолчанию удаляется первый нашедшийся элемент"""
         node = self.head
         preNode = Node(None)
         while node is not None:
-            if node == self.head and node.value == item:
+            if node == self.head and node.value == val:
                 self.head = node.next
-                if boolItem == False:
+                if all == False:
                     return
-            elif node.value != item:
+            elif node.value != val:
                 preNode = node
             else:
                 preNode.next = node.next
-                if boolItem == False:
+                if all == False:
                     return
-            node = node.next
+            node = node.next            
          
     #1.3    
     def clean(self):
-        #Очистка списка
-        self.head.value = None
-        self.head.next = None
+        """Очищает содержимое списка (создание пустого списка)"""
+        self.head = None
+        self.tail = None 
+        
     #1.4 
-    def find_all_nodes(self, item):
-        # Поиск всех узлов по значению
+    def find_all(self, val):
+        """Возвращает список узлов по заданному значению"""
         def iter(node, acc):
             if node is None:
                 return acc
-            if node.value == item:
-                acc.append(node.value)
+            if node.value == val:
+                #acc.append(node.value)
+                acc.append(node)
             return iter(node.next, acc)    
         return iter(self.head, [])
+    
     # 1.5    
-    def get_length(self):
-        # Дать длину списка
+    def len(self):
+        """Возвращает длину списка"""
         def iter(node, acc):
             if node is None:
                 return acc
             return iter(node.next, acc + 1)
         return iter(self.head, 0)
+    
     # 1.6
-    def insert(self, item, itemIns):
-        # Метод вставки узла после заданного узла
+    def insert(self, afterNode, newNode):
+        """Вставляет узел после заданного узла по значению"""
+        if self.head == None:
+            self.add_in_tail(Node(newNode))
         def iter(node):
             if node is None:
-                print("value missing")
                 return
-            if node.value == item:
-                newNode = Node(itemIns)
+            if node.value == afterNode:
+                new_node = Node(newNode)
                 temp = node.next
-                node.next = newNode
-                newNode.next = temp
+                node.next = new_node
+                new_node.next = temp
                 return
             return iter(node.next)    
         return iter(self.head)
     
-# 1.7
-def sum_s_list(s1, s2):
-    if s1.get_length() != s2.get_length():
-        return print("lists of different lengths")
+# 1.8
+def sum_list(s1, s2):
+    """Если длины списков равны, возвращает список, каждый элемент которого равен сумме соответствующих элементов входных списков."""
+    if s1.len() != s2.len():
+        return 
     def iter(node_1, node_2, acc):
         if node_1 is None:
             return acc
@@ -107,7 +110,7 @@ def sum_s_list(s1, s2):
     return iter(s1.head, s2.head, [] )
 
 def get_list_nodes(item):
-    # Получить список узлов (вспомогательная функция)
+    """Вспомогательная функция: возвращает список значений узлов (визуалицация списка)""" 
     def iter(node, acc):
         if node is None:
             return acc
@@ -115,105 +118,107 @@ def get_list_nodes(item):
         return iter(node.next, acc)    
     return iter(item.head, [])    
 
-def test_remove():
-    s_list_test_1 = LinkedList()
+#1.7
+class TestLinkedList(unittest.TestCase):
     
-    s_list_test_1.add_in_tail(Node(12))
-    s_list_test_1.add_in_tail(Node(73))
-    s_list_test_1.add_in_tail(Node(73))
-    s_list_test_1.add_in_tail(Node(12))
-    s_list_test_1.add_in_tail(Node(73))
-    s_list_test_1.add_in_tail(Node(73))
+    def setUp(self):
+        self.list_test = LinkedList()
+        self.list_test.add_in_tail(Node(12))
+        self.list_test.add_in_tail(Node(73))
+        self.list_test.add_in_tail(Node(73))
+        self.list_test.add_in_tail(Node(12))
+        self.list_test.add_in_tail(Node(73))
+        self.list_test.add_in_tail(Node(73))
     
-    s_list_test_1.remove(12, False)
-    if get_list_nodes(s_list_test_1) == [73, 73, 12, 73, 73]:
-        print("test remove(item, False) is OK")
-    else:
-        print("test remove(item, False) is FAIL")
+    def test_delete_1(self):
         
-    s_list_test_1.remove(12)
-    if get_list_nodes(s_list_test_1) == [73, 73, 73, 73]:
-        print("test remove(item, True) is OK")
-    else:
-        print("test remove(item, True) is FAIL")    
-  
-def test_clean():
-    s_list_test_3 = LinkedList()
-    s_list_test_3.add_in_tail(Node(12))
-    s_list_test_3.add_in_tail(Node(38))
-    s_list_test_3.add_in_tail(Node(12))
-    s_list_test_3.add_in_tail(Node(55))
-    s_list_test_3.clean()
-    if get_list_nodes(s_list_test_3) == [None]:
-        print("test_clean() is OK")
-    else:    
-        print("test_clean() is FAIL")
-
-def test_find_all_nodes():
-    s_list_test_4 = LinkedList()
-    s_list_test_4.add_in_tail(Node(12))
-    s_list_test_4.add_in_tail(Node(38))
-    s_list_test_4.add_in_tail(Node(12))
-    s_list_test_4.add_in_tail(Node(55))
-    if s_list_test_4.find_all_nodes(12) == [12, 12]:
-        print("test find_all_nodes(item) is OK")
-    else:
-        print("test find_all_nodes(item) is FAIL")
-
-def test_get_length():
-    s_list_test_5 = LinkedList()
-    s_list_test_5.add_in_tail(Node(12))
-    s_list_test_5.add_in_tail(Node(38))
-    s_list_test_5.add_in_tail(Node(12))
-    s_list_test_5.add_in_tail(Node(55))
-    
-    if s_list_test_5.get_length() == 4:
-        print("test get_length(item) is OK")
-    else:
-        print("test get_length(item) is FAIL")
-
-def test_insert():
-    s_list_test_6 = LinkedList()
-    s_list_test_6.add_in_tail(Node(12))
-    s_list_test_6.add_in_tail(Node(38))
-    s_list_test_6.add_in_tail(Node(12))
-    s_list_test_6.add_in_tail(Node(55))
-    s_list_test_6.insert(38, 73)
-    if get_list_nodes(s_list_test_6) == [12, 38, 73, 12, 55]:
-        print("test insert(item_1, item_2) is OK")
-    else:
-        print("test remove(item_1, item_2) is FAIL")
+        self.list_test.delete(12)
+        self.assertEqual(get_list_nodes(self.list_test), [73, 73, 12, 73, 73])
         
-def test_sum_s_list():
-    s_list_test_7 = LinkedList()
-    s_list_test_7.add_in_tail(Node(12))
-    s_list_test_7.add_in_tail(Node(38))
-    s_list_test_7.add_in_tail(Node(12))
-    s_list_test_7.add_in_tail(Node(55))
-    s_list_test_8 = LinkedList()
-    s_list_test_8.add_in_tail(Node(120))
-    s_list_test_8.add_in_tail(Node(550))
-    s_list_test_8.add_in_tail(Node(1280))
-    s_list_test_8.add_in_tail(Node(550))    
-    
-    if sum_s_list(s_list_test_7, s_list_test_8) == [132, 588, 1292, 605]:
-        print("test sum_s_list(item_1, item_2) is OK")
-    else:
-        print("test sum_s_list(item_1, item_2) is FAIL")
-
-test_remove()
-test_clean()
-test_find_all_nodes()
-test_get_length()
-test_insert()    
-test_sum_s_list()        
+    def test_delete_2(self):
         
-lst = LinkedList()
-lst.add_in_tail(Node(12))
-lst.add_in_tail(Node(14))
-lst.add_in_tail(Node(73))
-lst.add_in_tail(Node(11))
-lst.add_in_tail(Node(77))
-lst.add_in_tail(Node(58))
-
-print(next(lst))
+        self.list_test.delete(12, all)
+        self.assertEqual(get_list_nodes(self.list_test), [73, 73, 73, 73])
+        
+    def test_delete_3(self):
+        
+        self.list_test = LinkedList()
+        self.list_test.delete(12)
+        self.assertEqual(get_list_nodes(self.list_test), [])
+        
+        self.list_test = LinkedList()
+        self.list_test.add_in_tail(Node(12))
+        self.list_test.delete(12)
+        self.assertEqual(get_list_nodes(self.list_test), [])
+        
+        self.list_test = LinkedList()
+        self.list_test.add_in_tail(Node(12))
+        self.list_test.delete(10)
+        self.assertEqual(get_list_nodes(self.list_test), [12])        
+        
+    def test_clean(self):
+        
+        self.list_test.clean() 
+        self.assertEqual(self.list_test.head, None)
+        self.assertEqual(self.list_test.tail, None)
+        
+    def test_find_all(self):
+        
+        lst = self.list_test.find_all(12)
+        list_value = [node.value for node in lst]
+        self.assertEqual(list_value, [12, 12]) 
+        
+        list_test = LinkedList()
+        self.assertEqual(list_test.find_all(12), [])
+        
+        list_test = LinkedList()
+        list_test.add_in_tail(Node(12))
+        lst = list_test.find_all(12)
+        list_value = [node.value for node in lst]        
+        self.assertEqual(list_value, [12])        
+        
+    def test_len(self):
+        
+        self.assertEqual(self.list_test.len(), 6)
+        
+        list_test = LinkedList()
+        self.assertEqual(list_test.len(), 0)
+        
+    def test_insert(self):
+        
+        self.list_test.insert(73, 1000) 
+        self.assertEqual(get_list_nodes(self.list_test), [12, 73, 1000, 73, 12, 73, 73])
+        
+        list_test = LinkedList()
+        list_test.add_in_tail(Node(12))
+        list_test.insert(12, 1000)
+        self.assertEqual(get_list_nodes(list_test), [12, 1000])
+        
+        list_test = LinkedList()
+        list_test.insert(None, 100)
+        self.assertEqual(get_list_nodes(list_test), [100])
+        
+    def test_sum_list(self):
+            
+        list_test_1 = LinkedList()
+        list_test_1.add_in_tail(Node(12))
+        list_test_1.add_in_tail(Node(38))
+        list_test_1.add_in_tail(Node(12))
+        list_test_1.add_in_tail(Node(55))
+        list_test_2 = LinkedList()
+        list_test_2.add_in_tail(Node(120))
+        list_test_2.add_in_tail(Node(550))
+        list_test_2.add_in_tail(Node(1280))
+        list_test_2.add_in_tail(Node(550))    
+            
+        self.assertEqual(sum_list(list_test_1, list_test_2), [132, 588, 1292, 605])
+                    
+        list_test_2 = LinkedList()
+        list_test_2.add_in_tail(Node(120))
+        list_test_2.add_in_tail(Node(550))
+        list_test_2.add_in_tail(Node(1280))
+               
+        self.assertEqual(sum_list(list_test_1, list_test_2), None)
+        
+if __name__ == '__main__':
+    unittest.main() 
