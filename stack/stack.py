4b7a41a6ec5bd5b04e294aa54bb7d4d4053d4dbe
igbes <igbes900@gmail.com>
2019-04-04 22:12:49 +0800

    Stack / стек

--- stack/stack.py
+++ stack/stack.py
@@ -3,20 +3,24 @@ import unittest
 class Stack:
     def __init__(self):
         self.stack = []
-
+   
+    def size(self):
+        return len(self.stack)    
+        
     def pop(self):
-        if len(self.stack) == 0:
-            return None
-        return self.stack.pop()
+        if len(self.stack) != 0:
+            return self.stack.pop()
+        return None
+        
 
     def push(self, value):
         self.stack.append(value)
         return value
         
     def peak(self):
-        if len(self.stack) == 0:
-            return None
-        return self.stack[-1]
+        if len(self.stack) != 0:
+            return self.stack[-1]
+        return None
+        
 
-    def size(self):
-        return len(self.stack)
+    
