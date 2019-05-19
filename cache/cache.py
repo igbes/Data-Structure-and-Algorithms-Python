11333fcdd1fc32089272c180864f4eb99ac14aa0
igbes <igbes900@gmail.com>
2019-05-19 20:41:46 +0800

    Cache / кэш

--- cache/cache.py
+++ cache/cache.py
@@ -1,58 +1,61 @@
+
 class NativeCache:
-    def __init__(self, sz, stp):
+    
+    def __init__(self, sz):
         self.size = sz
-        self.step = stp
         self.slots = [None] * self.size
         self.values = [None] * self.size
         self.hits = [0] * self.size
         
     def hash_fun(self, value):
+        """Возвращает корректный индекс слота"""
         sum_code = 0
         for simbol in value:
             sum_code += ord(simbol)
         return sum_code % self.size
        
     def is_key(self, key):
-        # Проверяет, имеется ли в слотах ключ
+        """Возвращает True, если ключ имеется, иначе False"""
         slot = self.hash_fun(key)
-        for i in range(0, self.size * self.step):
-            if self.slots[slot] == None or self.slots[slot] == key:
-                return slot
-            slot = (slot + self.step) % self.size
-        return self._get_less()
-            
-    def find(self, key):
-        slot = self.hash_fun(key)
-        for i in range(0, self.size * self.step):
-            if self.slots[slot] == None:
-                return                 
-            if self.slots[slot] == key:
-                return slot            
-            slot = (slot + self.step) % self.size
-            
+        if self.slots[slot] == None:
+            return False
+        if key in self.slots[slot]:
+            return True
+        return False
+    
     def put(self, key, value):
-        slot_number = self.is_key(key)
-        if slot_number != None:
-            self.slots[slot_number] = key
-            self.values[slot_number] = value
-            self.hits[slot_number] += 1
+        """Гарантированно записывает заначение value по ключу key"""
+        slot = self.hash_fun(key)
+        if self.slots[slot] == None:
+            self.slots[slot] = []
+            self.values[slot] = []
+            self.hits[slot] = []
+        if self.is_key(key):
+            index = self.slots[slot].index(key)
+            self.values[slot][index] = value
+            self.hits[slot][index] += 1
+        else:
+            if len(self.slots[slot]) == 2:
+                min_value = min(self.hits[slot])
+                index = self.hits[slot].index(min_value)
+                self.hits[slot].remove(self.hits[slot][index])
+                self.values[slot].remove(self.values[slot][index])
+                self.slots[slot].remove(self.slots[slot][index])
+            self.slots[slot].append(key)
+            self.values[slot].append(value)  
+            self.hits[slot].append(1)
+    
+    def get(self, key):
+        """Возвращает value для key, 
+        или None если ключ не найден"""
+        slot = self.hash_fun(key)
+        if self.slots[slot] == None:
+            return None    
+        if key in self.slots[slot]:
+            index = self.slots[slot].index(key)
+            self.hits[slot][index] += 1
+            return self.values[slot][index]
+        else:
+            return None
         
-    def _get_less(self):
-        less = self.hits[0]
-        index = 0
-        for i in range(len(self.hits)):
-            if self.hits[i] < less:
-                less = self.hits[i]
-                index = i
-        self.hits[index] = 0        
-        return index       
-                   
-    def show_table_keys(self):
-        # Показывает массив ключей (вспомогательная функция)
-        return self.slots
-
-    def show_table_values(self):
-        # Показывает массив значений (вспомогательная функция)
-        return self.values
-    def show_table_hits(self):
-        return self.hits
+    
--- cache/test.py
+++ cache/test.py
@@ -4,44 +4,62 @@ import unittest
 class TestCache(unittest.TestCase):
     
     def setUp(self):
-        self.my_cache = NativeCache(17, 3)
-        self.my_cache.put("AA", "item 1")
-        self.my_cache.put("Bc", "item 2")
-        self.my_cache.put("dE", "item 3") 
-        self.my_cache.put("eD", "item 4")         
+        self.my_dict = NativeCache(17)
+        self.my_dict.put("AA", "item 1")
+        self.my_dict.put("Bc", "item 2")
+        self.my_dict.put("dE", "item 3") 
+        self.my_dict.put("eD", "item 4")         
         
     def test_hash_fun(self):
-        self.assertEqual(self.my_cache.hash_fun("dE"), 16)
-        self.assertEqual(self.my_cache.hash_fun("eD"), 16)
+        """Проверка создания индекса слота"""
+        self.assertEqual(self.my_dict.hash_fun("dE"), 16)
+        self.assertEqual(self.my_dict.hash_fun("eD"), 16)
     
     def test_put(self):
-        self.my_cache = NativeCache(5, 3)
-        self.my_cache.put("AA", "item 1")
-        self.my_cache.put("Bc", "item 2")
-        self.my_cache.put("dE", "item 3") 
-        self.my_cache.put("eD", "item 4") 
-        self.assertEqual(self.my_cache.show_table_keys(), ['AA', None, 'eD', 'Bc', 'dE'])
-        self.assertEqual(self.my_cache.show_table_values(), ['item 1', None, 'item 4', 'item 2', 'item 3']) 
-        self.my_cache.put("Bc", "item 5")
-        self.assertEqual(self.my_cache.show_table_keys(), ['AA', None, 'eD', 'Bc', 'dE'])
-        self.assertEqual(self.my_cache.show_table_values(), ['item 1', None, 'item 4', 'item 5', 'item 3'])
-        self.assertEqual(self.my_cache.show_table_hits(), [1, 0, 1, 2, 1])
-        self.my_cache.put("AA", "item 7")
-        self.my_cache.put("AA", "item 7")
-        self.my_cache.put("BB", "item 9")
-        # Проверка удаления наименее часто используемого элемента при отсутствии места в хеш-таблице,
-        # удаляется первый наименьший элемент и вставляется новый
-        self.assertEqual(self.my_cache.show_table_hits(), [3, 1, 1, 2, 1])
-        self.my_cache.put("TT", "item 100")
-        self.assertEqual(self.my_cache.show_table_keys(), ['AA', 'TT', 'eD', 'Bc', 'dE'])
-        self.assertEqual(self.my_cache.show_table_values(), ['item 7', 'item 100', 'item 4', 'item 5', 'item 3'])
-        self.assertEqual(self.my_cache.show_table_hits(), [3, 1, 1, 2, 1])
-
-    def test_find(self):
-        # Проверка поиска ключа по значению:
-        self.assertEqual(self.my_cache.find("dE"), 16)
-        self.assertEqual(self.my_cache.find("eD"), 2)
-        self.assertEqual(self.my_cache.find("DD"), None)
+        """Проверка создания пары ключ-значение"""
+        self.assertEqual(self.my_dict.slots, 
+            [None, None, None, None, None, None, None, None, None, None, None, ['AA'], ['Bc'], None, None, None, ['dE', 'eD']])
+        self.assertEqual(self.my_dict.values, 
+            [None, None, None, None, None, None, None, None, None, None, None, ['item 1'], ['item 2'], None, None, None, ['item 3', 'item 4']])
+        self.assertEqual(self.my_dict.hits, [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, [1], [1], 0, 0, 0, [1, 1]])        
+        
+        self.my_dict.put("AA", "item 100")
+        self.assertEqual(self.my_dict.slots, 
+            [None, None, None, None, None, None, None, None, None, None, None, ['AA'], ['Bc'], None, None, None, ['dE', 'eD']])
+        self.assertEqual(self.my_dict.values, 
+            [None, None, None, None, None, None, None, None, None, None, None, ['item 100'], ['item 2'], None, None, None, ['item 3', 'item 4']]) 
+        self.assertEqual(self.my_dict.hits, [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, [2], [1], 0, 0, 0, [1, 1]])  
+        
+        self.my_dict.put("eD", "item 200")
+        self.assertEqual(self.my_dict.slots, 
+            [None, None, None, None, None, None, None, None, None, None, None, ['AA'], ['Bc'], None, None, None, ['dE', 'eD']])
+        self.assertEqual(self.my_dict.values, 
+            [None, None, None, None, None, None, None, None, None, None, None, ['item 100'], ['item 2'], None, None, None, ['item 3', 'item 200']])
+        self.assertEqual(self.my_dict.hits, [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, [2], [1], 0, 0, 0, [1, 2]])  
+        
+        self.my_dict.put("Cf", "item 500")
+        self.assertEqual(self.my_dict.slots, 
+            [None, None, None, None, None, None, None, None, None, None, None, ['AA'], ['Bc'], None, None, None, ['eD', 'Cf']])
+        self.assertEqual(self.my_dict.values, 
+            [None, None, None, None, None, None, None, None, None, None, None, ['item 100'], ['item 2'], None, None, None, ['item 200', 'item 500']])
+        self.assertEqual(self.my_dict.hits, [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, [2], [1], 0, 0, 0, [2, 1]])       
+       
+    def test_is_key(self):
+        """Проверка наличия ключа в слоте"""
+        self.assertEqual(self.my_dict.is_key('AA'), True)
+        self.assertEqual(self.my_dict.is_key('eD'), True)  
+        self.assertEqual(self.my_dict.is_key('DD'), False) 
+        
+    def test_get(self):
+        """Проверка поиска значения по ключу"""
+        self.assertEqual(self.my_dict.get('dE'), 'item 3')
+        self.assertEqual(self.my_dict.hits, [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, [1], [1], 0, 0, 0, [2, 1]]) 
+        
+        self.assertEqual(self.my_dict.get("eD"), 'item 4')
+        self.assertEqual(self.my_dict.hits, [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, [1], [1], 0, 0, 0, [2, 2]])
+        
+        self.assertEqual(self.my_dict.get("DD"), None)
+        self.assertEqual(self.my_dict.hits, [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, [1], [1], 0, 0, 0, [2, 2]])
         
 if __name__ == '__main__':
-    unittest.main()       
+    unittest.main()        
