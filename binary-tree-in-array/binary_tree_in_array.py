4cc22e8577d1c8e107286153fae73b73ecf07fcc
igbes <igbes900@gmail.com>
2019-05-28 00:53:55 +0800

    Binary tree in array / бинарное дерево в виде массива

---
+++ binary-tree-in-array/binary_tree_in_array.py
@@ -0,0 +1,59 @@
+
+class aBST:
+    def __init__(self, depth):
+        self.depth = depth
+        self._length = self._get_len_array(depth)
+        self.Tree = [None] * self._length
+    
+    def _get_len_array(self, depth_tree):
+        """Возвращает количество узлов (длина списка)"""
+        number_in_level = 1
+        total = 1
+        for level in range(depth_tree):
+            number_in_level *= 2
+            total += number_in_level
+        return total
+    
+    def FindKeyIndex(self, key):
+        """Поиск заданного узла по значению. Поиск ведётся от корня дерева.
+        Возвращает None, если значение не найдено и достигнут предел по глубине,
+        возвращает -index, если на месте предполагаемого нахождения искомого 
+        елемента находится None"""
+        def iter(index, level):
+            if key == self.Tree[index]: 
+                return index
+            if level == self.depth:
+                return 
+            if key < self.Tree[index]:
+                if self.Tree[2 * index + 1] == None:
+                    return - (2 * index + 1)
+                return iter(2 * index + 1, level + 1)
+            else:
+                if self.Tree[2 * index + 2] == None:
+                    return - (2 * index + 2)
+                return iter(2 * index + 2, level + 1)
+        return iter(0, 0) 
+    
+    def AddKey(self, key):
+        """Добавление узла в дерево"""
+        if self.Tree[0] == None:
+            self.Tree[0] = key
+            return 0
+        def iter(index, level):
+            if level == self.depth:
+                return -1           
+            if key < self.Tree[index]:
+                if self.Tree[2 * index + 1] == None:
+                    self.Tree[2 * index + 1] = key
+                    return 
+                else:       
+                    return iter(2 * index + 1, level + 1)
+            else:
+                if key > self.Tree[index]:
+                    if self.Tree[2 * index + 2] == None:
+                        self.Tree[2 * index + 2] = key
+                        return 
+                    else:       
+                        return iter(2 * index + 2, level + 1)                 
+        return iter(0, 0) 
+    
---
+++ binary-tree-in-array/test.py
@@ -0,0 +1,49 @@
+import unittest
+from binary_tree_in_array import aBST
+
+class TestaBST(unittest.TestCase):
+    
+    def setUp(self):
+        """Создание дерева из 15 злементов, начиная от корня,
+        глубиной, равной 3"""
+        self.tree2 = aBST(3)    
+        self.tree2.AddKey(50)
+        self.tree2.AddKey(25)
+        self.tree2.AddKey(75)
+        self.tree2.AddKey(37)
+        self.tree2.AddKey(62)
+        self.tree2.AddKey(84)
+        self.tree2.AddKey(31)
+        self.tree2.AddKey(43)
+        self.tree2.AddKey(55)
+        self.tree2.AddKey(92)          
+        
+    def test_AddKey(self):
+        """Проверка создания дерева из 15 элементов путём добавления новых узлов"""
+        self.assertEqual(self.tree2.Tree, [50, 25, 75, None, 37, 62, 84, None, None, 31, 43, 55, None, None, 92])
+        
+    # Проверка поиска элемента по ключу:
+        
+    def test_FindKeyIndex_1(self):
+        """Проверка на частично заполненном дереве"""
+        self.assertEqual(self.tree2.FindKeyIndex(50), 0)
+        self.assertEqual(self.tree2.FindKeyIndex(92), 14)
+        self.assertEqual(self.tree2.FindKeyIndex(37), 4)
+        self.assertEqual(self.tree2.FindKeyIndex(20), -3)
+        self.assertEqual(self.tree2.FindKeyIndex(66), -12)
+       
+    def test_FindKeyIndex_2(self):
+        """Продолжение проверки на полностью заполненном дереве (без пустых узлов)"""
+        self.tree2.AddKey(23)
+        self.tree2.AddKey(20)
+        self.tree2.AddKey(21)
+        self.tree2.AddKey(65)
+        self.tree2.AddKey(80)
+        
+        self.assertEqual(self.tree2.FindKeyIndex(75), 2)
+        self.assertEqual(self.tree2.FindKeyIndex(22), None) 
+        self.assertEqual(self.tree2.FindKeyIndex(66), None) 
+        self.assertEqual(self.tree2.FindKeyIndex(83), None) 
+    
+if __name__ == '__main__':
+    unittest.main()  
